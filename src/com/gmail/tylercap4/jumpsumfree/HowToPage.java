97d7d69457e587a4f7593be39e1e36714a415540
Tyler Cap <tcap4@yahoo.com>
2014-12-26 18:26:51 -0600

    initial commit

---
+++ src/com/gmail/tylercap4/jumpsumfree/HowToPage.java
@@ -0,0 +1,15 @@
+package com.gmail.tylercap4.jumpsumfree;
+
+import com.gmail.tylercap4.jumpsum.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HowToPage extends Activity {
+
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.how_to);
+	}
+}
