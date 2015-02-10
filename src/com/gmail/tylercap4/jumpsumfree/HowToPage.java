f807380a83cbe78e5f8d0b805bcfa9963e11e32a
Tyler <Tyler@localhost>
2015-02-09 21:35:36 -0600

    switched back to admob for ads

--- src/com/gmail/tylercap4/jumpsumfree/HowToPage.java
+++ src/com/gmail/tylercap4/jumpsumfree/HowToPage.java
@@ -1,15 +1,16 @@
 package com.gmail.tylercap4.jumpsumfree;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
-import android.widget.FrameLayout;
 
 public class HowToPage extends Activity 
 {
-	//private com.google.android.gms.ads.AdView adMobView;
-	private com.adsdk.sdk.banner.AdView mobFoxAdView;
+	private AdView adMobView;
 
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,39 +19,27 @@ public class HowToPage extends Activity
         getActionBar().setDisplayHomeAsUpEnabled(true);
 
         // Load the banner ad
-        //adMobView = (AdView) findViewById(R.id.adView);
-        //adMobView.loadAd(new AdRequest.Builder().build());
-        
-        // Load the banner ad
-        FrameLayout layout = (FrameLayout) findViewById(R.id.banner_layout);
-        mobFoxAdView = new com.adsdk.sdk.banner.AdView(this, "http://my.mobfox.com/request.php", getString(R.string.mob_fox_publisher_id), true, true);
-        mobFoxAdView.setAdspaceWidth(320); // Optional, used to set the custom size of banner placement. Without setting it, the SDK will use default size of 320x50 or 300x50 depending on device type.
-        mobFoxAdView.setAdspaceHeight(50);  
-        mobFoxAdView.setAdspaceStrict(false); // Optional, tells the server to only supply banner ads that are exactly of the desired size. Without setting it, the server could also supply smaller Ads when no ad of desired size is available.
-        // mobFoxAdView.setAdListener(this);
-        layout.addView(mobFoxAdView);
+        adMobView = (AdView) findViewById(R.id.adView);
+        adMobView.loadAd(new AdRequest.Builder().build());
 	}
 	
     @Override
     protected void onResume(){
     	super.onResume();
-        //adMobView.resume();
-    	mobFoxAdView.resume();
+        adMobView.resume();
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        //adMobView.pause();
-    	mobFoxAdView.pause();
+        adMobView.pause();
     }
 
     
     @Override
     protected void onPause(){
     	super.onPause();
-        //adMobView.pause();
-    	mobFoxAdView.pause();
+        adMobView.pause();
     }
     
     @Override
